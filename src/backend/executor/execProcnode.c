f8e9ac79c8916d414dd25396b3baae81c5fb4849
Claudia Exeler <cl.ex@gmx.de>
2014-01-07 16:25:39 +0100

    fix bug with distinct value counting

--- src/backend/executor/execProcnode.c
+++ src/backend/executor/execProcnode.c
@@ -552,9 +552,9 @@ ExecProcNode(PlanState *node) {
 							|| piggyback->resultStatistics->columnStatistics[i].minValue == INT_MAX)
 						piggyback->resultStatistics->columnStatistics[i].minValue = value;
 					if (value > piggyback->resultStatistics->columnStatistics[i].maxValue
-							|| piggyback->resultStatistics->columnStatistics[i].maxValue == NULL)
+							|| piggyback->resultStatistics->columnStatistics[i].maxValue == INT_MIN)
 						piggyback->resultStatistics->columnStatistics[i].maxValue = value;
-					if (piggyback->resultStatistics->columnStatistics[i].isNumeric == -2) {
+					if (piggyback->resultStatistics->columnStatistics[i].distinct_status == -2) {
 						hashset_add_numeric(piggyback->distinctValues[i], value);
 					}
 					break;
