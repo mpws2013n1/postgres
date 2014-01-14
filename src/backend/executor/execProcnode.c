f051d83c8bad985acfd020a3670d717f9479acae
Cathleen Ramson <cathleen.ramson@student.hpi.uni-potsdam.de>
2014-01-14 15:45:26 +0100

    extracted code for finding special selections

--- src/backend/executor/execProcnode.c
+++ src/backend/executor/execProcnode.c
@@ -150,6 +150,10 @@ ExecInitNode(Plan *node, EState *estate, int eflags) {
 	List *subps;
 	ListCell *l;
 
+	// Pointers that are necessary for specific node types like SeqScan.
+	SeqScanState* resultAsScanState;
+	AggState* resultAsAggState;
+	int tableOid = -1;
 	/*
 	 * do nothing when we get to the end of a leaf on tree.
 	 */
@@ -197,62 +201,15 @@ ExecInitNode(Plan *node, EState *estate, int eflags) {
 		 * scan nodes
 		 */
 	case T_SeqScan:
-		result = (PlanState *) ExecInitSeqScan((SeqScan *) node, estate,
+		resultAsScanState = ExecInitSeqScan((SeqScan *) node, estate,
 				eflags);
+		result = (PlanState *) resultAsScanState;
 
-		if (result->qual) {
-			int opno = ((OpExpr*) ((ExprState*) linitial(result->qual))->expr)->opno;
-
-			if(opno == 94 || opno == 96 || opno == 410 || opno == 416 || opno == 1862 || opno == 1868 || opno == 15 || opno == 532 || opno == 533) { // it is a equality like number_of_tracks = 3
-				be_PGAttDesc *columnData = (be_PGAttDesc*) malloc(sizeof(be_PGAttDesc));
-				int numberOfAttributes = result->plan->targetlist->length;
-				int tableOid = -1;
-				SeqScanState* resultAsScanState = ((SeqScanState*)result);
-				if (resultAsScanState)
-				{
-					tableOid = ((SeqScanState*)result)->ss_currentRelation->rd_id;
-				}
-
-				int *minAndMaxAndAvg = (int*) malloc(sizeof(int));
-				int columnId = ((Var*) ((OpExpr*) ((ExprState*) linitial(result->qual))->expr)->args->head->data.ptr_value)->varattno;
-				minAndMaxAndAvg = &(((Const*) ((OpExpr*) ((ExprState*) linitial(result->qual))->expr)->args->tail->data.ptr_value)->constvalue);
-
-				columnData->srccolumnid = columnId;
-
-				// we always set the type to 8byte-integer because we don't need a detailed differentiation
-				columnData->typid = 20;
-
-				// TODO: write this in a method that returns i for better readability
-				int i = 0;
-				for (; i < piggyback->numberOfAttributes; i++)
-				{
-					if (tableOid == piggyback->resultStatistics->columnStatistics[i].columnDescriptor->srctableid
-							&& columnData->srccolumnid == piggyback->resultStatistics->columnStatistics[i].columnDescriptor->srccolumnid)
-						break;
-				}
-
-				// only write values, if the selected field is part of the result table
-				if (i < piggyback->numberOfAttributes)
-				{
-					piggyback->resultStatistics->columnStatistics[i].columnDescriptor = columnData;
-					piggyback->resultStatistics->columnStatistics[i].isNumeric = 1;
-					piggyback->resultStatistics->columnStatistics[i].maxValue = *minAndMaxAndAvg;
-					piggyback->resultStatistics->columnStatistics[i].minValue = *minAndMaxAndAvg;
-					piggyback->resultStatistics->columnStatistics[i].mostFrequentValue = *minAndMaxAndAvg;
-					piggyback->resultStatistics->columnStatistics[i].distinct_status = 1;
-
-					// the meta data for this column ist complete and should not be calculated again
-					piggyback->resultStatistics->columnStatistics[i].n_distinctIsFinal = 1;
-					piggyback->resultStatistics->columnStatistics[i].minValueIsFinal = 1;
-					piggyback->resultStatistics->columnStatistics[i].maxValueIsFinal = 1;
-					piggyback->resultStatistics->columnStatistics[i].mostFrequentValueIsFinal = 1;
-				}
-				else
-				{
-					printf("there are statistics results from the selection that are not part of the result table\n");
-				}
-			}
+		if (resultAsScanState)
+		{
+			tableOid = resultAsScanState->ss_currentRelation->rd_id;
 		}
+		LookForFilterWithEquality(result, tableOid);
 		break;
 
 	case T_IndexScan:
@@ -345,7 +302,14 @@ ExecInitNode(Plan *node, EState *estate, int eflags) {
 		break;
 
 	case T_Agg:
-		result = (PlanState *) ExecInitAgg((Agg *) node, estate, eflags);
+		resultAsAggState = ExecInitAgg((Agg *) node, estate, eflags);
+		result = (PlanState *) resultAsAggState;
+
+		if (resultAsAggState)
+		{
+			//tableOid = resultAsAggState->ss.ss_currentRelation->rd_id;
+		}
+		//LookForFilterWithEquality(result, tableOid);
 		break;
 
 	case T_WindowAgg:
@@ -401,6 +365,64 @@ ExecInitNode(Plan *node, EState *estate, int eflags) {
 		result->instrument = InstrAlloc(1, estate->es_instrument);
 
 	return result;
+}
+
+void
+LookForFilterWithEquality(PlanState* result, Oid tableOid)
+{
+	if (result->qual) {
+		int opno = ((OpExpr*) ((ExprState*) linitial(result->qual))->expr)->opno;
+
+		if(opno == 94 || opno == 96 || opno == 410 || opno == 416 || opno == 1862 || opno == 1868 || opno == 15 || opno == 532 || opno == 533) { // it is a equality like number_of_tracks = 3
+			be_PGAttDesc *columnData = (be_PGAttDesc*) malloc(sizeof(be_PGAttDesc));
+			int numberOfAttributes = result->plan->targetlist->length;
+			/*int tableOid = -1;
+			SeqScanState* resultAsScanState = ((SeqScanState*)result);
+			if (resultAsScanState)
+			{
+				tableOid = ((SeqScanState*)result)->ss_currentRelation->rd_id;
+			}*/
+
+			int *minAndMaxAndAvg = (int*) malloc(sizeof(int));
+			int columnId = ((Var*) ((OpExpr*) ((ExprState*) linitial(result->qual))->expr)->args->head->data.ptr_value)->varattno;
+			minAndMaxAndAvg = &(((Const*) ((OpExpr*) ((ExprState*) linitial(result->qual))->expr)->args->tail->data.ptr_value)->constvalue);
+
+			columnData->srccolumnid = columnId;
+
+			// we always set the type to 8byte-integer because we don't need a detailed differentiation
+			columnData->typid = 20;
+
+			// TODO: write this in a method that returns i for better readability
+			int i = 0;
+			for (; i < piggyback->numberOfAttributes; i++)
+			{
+				if (tableOid == piggyback->resultStatistics->columnStatistics[i].columnDescriptor->srctableid
+						&& columnData->srccolumnid == piggyback->resultStatistics->columnStatistics[i].columnDescriptor->srccolumnid)
+					break;
+			}
+
+			// only write values, if the selected field is part of the result table
+			if (i < piggyback->numberOfAttributes)
+			{
+				piggyback->resultStatistics->columnStatistics[i].columnDescriptor = columnData;
+				piggyback->resultStatistics->columnStatistics[i].isNumeric = 1;
+				piggyback->resultStatistics->columnStatistics[i].maxValue = *minAndMaxAndAvg;
+				piggyback->resultStatistics->columnStatistics[i].minValue = *minAndMaxAndAvg;
+				piggyback->resultStatistics->columnStatistics[i].mostFrequentValue = *minAndMaxAndAvg;
+				piggyback->resultStatistics->columnStatistics[i].distinct_status = 10;
+
+				// the meta data for this column ist complete and should not be calculated again
+				piggyback->resultStatistics->columnStatistics[i].n_distinctIsFinal = 1;
+				piggyback->resultStatistics->columnStatistics[i].minValueIsFinal = 1;
+				piggyback->resultStatistics->columnStatistics[i].maxValueIsFinal = 1;
+				piggyback->resultStatistics->columnStatistics[i].mostFrequentValueIsFinal = 1;
+			}
+			else
+			{
+				printf("there are statistics results from the selection that are not part of the result table\n");
+			}
+		}
+	}
 }
 
 /* ----------------------------------------------------------------
